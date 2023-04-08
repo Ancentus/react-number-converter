f05445dac78ecb30aa124b5ec01832dc2932f6c4
Ancentus <mancentus7@gmail.com>
2023-04-08 18:25:34 +0300

    create number inputs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import NumbersForm from "./components/NumbersForm";
 
 function App() {
   return (
     <div className="App">
-      
+      <NumbersForm/>
     </div>
   );
 }
