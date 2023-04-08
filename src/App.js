b4f14b1368286af1636797e743dd8b03751c8a2f
Ancentus <mancentus7@gmail.com>
2023-04-08 19:20:20 +0300

    Style the app

--- src/App.js
+++ src/App.js
@@ -63,31 +63,33 @@ function App() {
   };
 
   return (
-    <form>
-          <div className="form-inner">
-              <h2>Number Conversion</h2>
-              <div className="form-group">
-                  <label htmlFor="decimal">Decimal:</label>
-                  <input type="number" id="decimal"
-                   value={decimal} onChange={handleDecimalChange} />
-              </div>
-              <div className="form-group">
-                  <label htmlFor="binary">Binary:</label>
-                  <input type="text" id="binary"
-                   value={binary} onChange={handleBinaryChange} />
-              </div>
-              <div className="form-group">
-                  <label htmlFor="hexadecimal">Hexadecimal:</label>
-                  <input type="text" id="hexadecimal"
-                   value={hexadecimal} onChange={handleHexadecimalChange} />
-              </div>
-              <div className="form-group">
-                  <label htmlFor="octal">Octal:</label>
-                  <input type="text" id="octal"
-                   value={octal} onChange={handleOctalChange} />
-              </div>
+    <div className="App">
+      <form>
+        <div className="form-inner">
+          <h2>Number Conversion</h2>
+          <div className="form-group">
+            <label htmlFor="decimal">Decimal:</label>
+            <input type="number" id="decimal"
+              value={decimal} onChange={handleDecimalChange} />
           </div>
+          <div className="form-group">
+            <label htmlFor="binary">Binary:</label>
+            <input type="text" id="binary"
+              value={binary} onChange={handleBinaryChange} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="hexadecimal">Hexadecimal:</label>
+            <input type="text" id="hexadecimal"
+              value={hexadecimal} onChange={handleHexadecimalChange} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="octal">Octal:</label>
+            <input type="text" id="octal"
+              value={octal} onChange={handleOctalChange} />
+          </div>
+        </div>
       </form>
+    </div>
   );
 }
 
